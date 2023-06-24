25d3da534fe148718b58be88062f5fa03773cc75
chkrishna142 <chkrishna142@gmail.com>
2023-06-24 17:43:25 +0530

    founded doubled target combination

--- combination-array.java
+++ combination-array.java
@@ -49,6 +49,62 @@ import java.util.*;
         return mergedArray;
     }
     
+    public static List<List<Integer>> findFourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Sorting the array to handle duplicates more easily
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            // Avoid duplicates in the outer loop
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+                // Avoid duplicates in the second loop
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        List<Integer> combination = new ArrayList<>();
+                        combination.add(nums[i]);
+                        combination.add(nums[j]);
+                        combination.add(nums[left]);
+                        combination.add(nums[right]);
+                        result.add(combination);
+
+                        // Avoid duplicates in the inner loop
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    
     public static int[][] findSecondCombination(int[] nums, int target) {
         int doubledTarget = target * 2;
         return findPairs(nums, doubledTarget);
@@ -67,7 +123,8 @@ import java.util.*;
            int[] mergedArray = mergeAndSort(result);
         System.out.println("Merge Into a single Array : " + Arrays.toString(mergedArray));
         
-         int[][] secondResult = findSecondCombination(nums, target);
-        System.out.println("Second Combination For \"" + (target * 2) + "\" : " + Arrays.deepToString(secondResult));
+    List<List<Integer>> doubleArray = findFourSum(nums, target*2);
+
+        System.out.println("Output: " + doubleArray);
     }
 }
